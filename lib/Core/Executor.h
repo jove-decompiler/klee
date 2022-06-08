254b0908c4a694d201d56fd42aab0d56fffb3788
Mikhail <mishok2503@mail.ru>
2022-06-08 16:36:28 +0300

    Inline asm external call

--- lib/Core/Executor.h
+++ lib/Core/Executor.h
@@ -65,6 +65,7 @@ namespace klee {
   class ExternalDispatcher;
   class Expr;
   class InstructionInfoTable;
+  class KCallable;
   struct KFunction;
   struct KInstruction;
   class KInstIterator;
@@ -253,7 +254,7 @@ private:
 
   void callExternalFunction(ExecutionState &state,
                             KInstruction *target,
-                            llvm::Function *function,
+                            KCallable *callable,
                             std::vector< ref<Expr> > &arguments);
 
   ObjectState *bindObjectInState(ExecutionState &state, const MemoryObject *mo,
