a3e6d013a1dcda439a69fc83cf147f3b3c9196d8
Daniel Schemmel <daniel@schemmel.net>
2023-03-24 15:05:43 +0000

    use unique_ptr all throughout the solver chain

--- lib/Core/Executor.h
+++ lib/Core/Executor.h
@@ -111,7 +111,7 @@ private:
   Searcher *searcher;
 
   ExternalDispatcher *externalDispatcher;
-  TimingSolver *solver;
+  std::unique_ptr<TimingSolver> solver;
   MemoryManager *memory;
   std::set<ExecutionState*, ExecutionStateIDCompare> states;
   StatsTracker *statsTracker;
