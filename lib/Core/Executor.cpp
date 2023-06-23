1eee9ffe43d598e05a9aae6492c0fad578b2a1e0
Cristian Cadar <c.cadar@imperial.ac.uk>
2023-06-23 22:44:49 +0100

    Fixed a couple of spelling issues in the help menu

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -211,7 +211,7 @@ cl::opt<bool> SuppressExternalWarnings(
 cl::opt<bool> AllExternalWarnings(
     "all-external-warnings",
     cl::init(false),
-    cl::desc("Issue a warning everytime an external call is made, "
+    cl::desc("Issue a warning every time an external call is made, "
              "as opposed to once per function (default=false)"),
     cl::cat(ExtCallsCat));
 
@@ -324,7 +324,7 @@ cl::list<StateTerminationType> ExitOnErrorType(
         clEnumValN(StateTerminationType::NullableAttribute, "NullableAttribute",
                    "Violation of nullable attribute detected"),
         clEnumValN(StateTerminationType::User, "User",
-                   "Wrong klee_* functions invocation")),
+                   "Wrong klee_* function invocation")),
     cl::ZeroOrMore,
     cl::cat(TerminationCat));
 
